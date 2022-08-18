8beb8268d4d0cbda923e3901e0b238abbe124b65
unknown <wairreb@gmail.com>
2022-08-18 12:44:31 +0300

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as Header } from './components/Headernts/Header';
+export { default as Header } from './components/Header';
 export { default as Footer } from './components/Footer';
-export { default as Home } from './components/Homeonents/Home';
-export { default as About} from './components/Aboutnents/About';
+export { default as Home } from './component/Home';
+export { default as About} from './components/About';
