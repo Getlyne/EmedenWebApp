dcd2e0317459b592ef50bee207fd36938a204646
unknown <wairreb@gmail.com>
2022-08-18 12:42:49 +0300

    commit changes

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer/Footer';
-export { default as Home } from './Home';
-export { default as About} from './About';
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './components/Headernts/Header';
+export { default as Footer } from './components/Footer';
+export { default as Home } from './components/Homeonents/Home';
+export { default as About} from './components/Aboutnents/About';
